9e041263aa40c84b15a728ce4927717aec2d4d8d
Jacques-Drumond <jacques.junio@hotmail.com>
2022-04-05 15:15:29 -0300

    adding mysql database

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+var mysql = require('mysql');
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "loginnodejs"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
 
 // sendFile will go here
 app.use( express.static( __dirname + '/public' ));
